4679756a88dc322d8fd62e1b7a35f9aafe9a3b2a
coatesmike <mrc13183@gmail.com>
2018-08-10 15:05:58 -0400

    made a directory called mce and added ehrenfest.py

---
+++ nomad/integrals/ehrenfest.py
@@ -0,0 +1,21 @@
+""" Computes the Multiconfigurational Ehrenfest integrals 
+for trajectories on multiple adiabatic electronic states 
+"""
+
+import numpy as np
+import nomad.compiled.nuclear_gaussian as nuclear
+
+def traj_ocerlap():
+	pass
+
+def nuclear_overlap():
+	pass
+
+def s_integral():
+	pass
+
+def t_integral():
+	pass
+
+def v_integral():
+	pass
