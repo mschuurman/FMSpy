34afa83ff23542bdf6ba3fe50eec386a7f1d627f
coatesmike <mrc13183@gmail.com>
2018-08-20 15:45:07 -0400

    Made tentative changes to ehrenfest_global.py

---
+++ nomad/integrals/ehrenfest_bra_ket.py
@@ -0,0 +1,10 @@
+"""
+Compute Bra-ket averaged Taylor expansion integrals over trajectories
+traveling on adiabataic potentials for the multiconfigurational
+Ehrenfest method. 
+"""
+import numpy as np
+import nomad.core.glbl as glbl
+import nomad.compiled.nuclear_gaussian as nuclear
+import nomad.compiled.vibronic_gaussian as vibronic
+
--- nomad/integrals/ehrenfest_global.py
+++ nomad/integrals/ehrenfest_global.py
@@ -1,14 +1,35 @@
 """ Computes the Multiconfigurational Ehrenfest integrals 
-for trajectories on multiple adiabatic electronic states 
+for trajectories on multiple adiabatic electronic states
+
+ref: Makhov, D. V.; Symonds, C.; Fernandez-Alberti, S.; Shalashilin, D. V. Chemical Physics 2017, 493, 200-218.
+doi: https://doi.org/10.1016/j.chemphys.2017.04.003  
 """
 
 import numpy as np
-import nomad.compiled.nuclear_gaussian as nuclear
+import nomad.compiled.nuclear_gaussian_ccs as nuclear
 
-def traj_ocerlap():
-	pass
+def elec_overlap(t1, t2):
+	"""Returns < Phi | Phi' >, the electronic overlap integral ie. Kronecker delta"""
+	if t1.state() == t2.state():
+        return 1.
+    else:
+        return 0.
 
-def nuclear_overlap():
+def traj_overlap(t1, t2, nuc_ovrlp=None):
+    """Returns < Psi | Psi' >, the overlap integral of two trajectories.
+
+    The bra and ket functions for the s_integral may be different
+    (i.e. pseudospectral/collocation methods).
+    """
+    return s_integral(t1, t2, nuc_ovrlp)
+
+def nuc_overlap(t1, t2):
+    """ Returns < Chi | Chi' >, the nuclear overlap integral of two trajectories"""
+    return nuclear.overlap(t1.phase(),t1.widths(),t1.x(),t1.p(),
+                           t2.phase(),t2.widths(),t2.x(),t2.p())
+
+def a_dot():
+	"""This allows for the propagation of the electronic state weights a(t) for a single EBF."""
 	pass
 
 def s_integral():
@@ -17,5 +38,3 @@ def s_integral():
 def t_integral():
 	pass
 
-def v_integral():
-	pass
